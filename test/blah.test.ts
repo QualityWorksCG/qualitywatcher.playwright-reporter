d0e53261585581ff0039b059431f55dacfebb86a
NyranMoodie <nyranmoodie@gmail.com>
2022-07-24 11:12:38 -0500

    add defualt test file

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,13 +1,5 @@
-import { test, expect } from '@playwright/test';
-
-test('[S100C1] Verify that the user can navigate to the getting started page', async ({
-  page,
-}) => {
-  // Go to https://playwright.dev/
-  await page.goto('https://playwright.dev/');
-  // Click text=Get started
-  await page.locator('text=Get started').click();
-  await expect(page).toHaveURL('https://playwright.dev/docs/intro');
-  // Click h1:has-text("Getting started")
-  await page.locator('h1:has-text("Getting started")').click();
+describe('blah', () => {
+  it('works', () => {
+    expect(1).toEqual(1);
+  });
 });
